1b796e389ef3a42df562d0c9193e8215f6fc37ff
Erik Kalviainen <erik@locl.co>
2016-04-09 11:44:48 -0400

    Add pizzabotexample

---
+++ examples/pizzabot/pizza_confirmation.py
@@ -0,0 +1,24 @@
+from meya import Component
+
+
+class PizzaConfirmation(Component):
+
+    def start(self):
+        # read the order and make lower case
+        order = self.db.request.get('pizza_type') or ""
+        order = order.lower()
+        print order
+
+        # conditional
+        if "anchovies" in order:
+            text = "Sorry, we don't serve anchovies"
+        elif "hawaiian" in order:
+            text = "Are you nuts? Don't put fruit on pizza!"
+        elif order:
+            text = "Thanks for your order! Pizza: {}".format(order)
+        else:
+            text = "No order received :("
+
+        # respond to the user
+        message = self.create_message(text=text)
+        return self.respond(message=message, action="next")
---
+++ examples/pizzabot/readme.md
@@ -0,0 +1,9 @@
+# PizzaBot example
+
+Try me here: https://meya.ai/pizzabot
+
+Say 'hi' to start.
+
+There are two examples:
+1. pizza_flow_simple.yaml: no custom components, uses mustache syntax inline
+2. pizza_flow_custom.yaml: a custom component that reads from datastore
