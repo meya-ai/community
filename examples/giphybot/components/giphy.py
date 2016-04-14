a7274571f036f1f84a608681b277405099cbb1ef
Erik Kalviainen <erik@locl.co>
2016-04-14 18:18:32 -0400

    Add giphy bot example

---
+++ examples/giphybot/components/giphy.py
@@ -0,0 +1,21 @@
+from meya import Component
+import requests
+
+API_KEY = "dc6zaTOxFJmzC"
+API_URL = "http://api.giphy.com/v1/gifs/random?tag={tag}&api_key={key}"
+
+
+class Giphy(Component):
+    """Outputs a random giphy url based on passed in tag as either
+    a property or request db"""
+
+    def start(self):
+        tag = self.properties.get('tag') or \
+            self.db.request.get('tag') or "funny"
+
+        response = requests.get(API_URL.format(tag=tag, key=API_KEY))
+        gif = response.json()['data']['url']
+
+        message = self.create_message(text=gif)
+
+        return self.respond(message=message, action="next")
